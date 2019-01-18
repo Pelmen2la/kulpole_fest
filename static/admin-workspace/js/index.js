b349ada81f1c96ce3913e546344ad9b6aa580781
Pelmen2la <pelmengi12>
2019-01-18 13:13:53 +0300

    - Переименовал файл стора - Добавлена иконка красного креста

--- static/admin-workspace/js/index.js
+++ static/admin-workspace/js/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import router from './router';
-import store from './vue-store';
+import store from './app-store';
 import App from './../vue-components/App';
 import './material-ui-init';
 import './../scss/main.scss';
