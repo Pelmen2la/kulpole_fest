4563381886143aa6fd7cb0596d956b2ca672a67f
Gavrilin Petr <pelmenfrom2la@gmail.com>
2019-04-22 14:11:07 +0300

    Добавлены отдельные ссылки на скачку документов и на чтение с главной страницы

--- static/landing/js/pages/main.js
+++ static/landing/js/pages/main.js
@@ -1,1 +1,22 @@
 import './../../scss/pages/main.scss'
+
+
+(window.mainPageAppModule = function() {
+    function init() {
+        checkNavUrl();
+    };
+
+    function checkNavUrl() {
+        const navTo = location.href.match(/#\S+/);
+        if(navTo) {
+            const link = document.querySelector(`[href="${navTo}"]`);
+            if(link) {
+                document.body.scrollTop = link.getBoundingClientRect().top;
+            }
+        }
+    };
+
+    return {
+        init
+    };
+}().init());
